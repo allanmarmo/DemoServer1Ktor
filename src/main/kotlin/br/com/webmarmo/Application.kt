f98f13f446452018d97670c26b3b7b310ed0fb61
allanmarmo <allanmarmo@gmail.com>
2021-12-15 18:57:46 -0300

    11. Static Contente

--- src/main/kotlin/br/com/webmarmo/Application.kt
+++ src/main/kotlin/br/com/webmarmo/Application.kt
@@ -1,30 +1,62 @@
 package br.com.webmarmo
 
 import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.serialization.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import kotlinx.serialization.Serializable
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
+        install(ContentNegotiation) {
+            json()
+        }
         module()
-        module2()
     }.start(wait = true)
 }
 
 fun Application.module() {
-    routing {
+    install(Routing) {
         get("/") {
             call.respondText { "Hello, World!" }
+        }
+        get("/users/{username}") {
+            val username = call.parameters["username"]
+            val header = call.request.headers["Connection"]
+            if (username == "Admin") {
+                call.response.header(name = "CustomHeader", "Admin")
+                call.respond(message = "Hello Admin", status = HttpStatusCode.OK)
+            }
+            call.respondText("Greetings, $username with header: $header")
+        }
+        get("/user") {
+            val name = call.request.queryParameters["name"]
+            val age = call.request.queryParameters["age"]
+            call.respondText("Hi, my name is $name, I´m $age years old!")
+        }
+        get("/person") {
+            try {
+                val person = Person(name = "Allan Marmo", age = 43)
+                call.respond(message = person, status = HttpStatusCode.OK)
+            } catch (e: Exception) {
+                call.respond(message = "${e.message}", status = HttpStatusCode.BadRequest)
+            }
+        }
+        get("/redirect") {
+            call.respondRedirect("/moved", permanent = false)
+        }
+        get("/moved") {
+            call.respondText("You have been successfully redirected!")
         }
     }
 }
 
-fun Application.module2() {
-    routing {
-        get("/book") {
-            call.respondText { "Hello, World 2!" }
-        }
-    }
-}
+@Serializable
+data class Person(
+    val name: String,
+    val age: Int
+)
